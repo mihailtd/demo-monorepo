5973cfb1500c6268fb85284ba3f472789dc39534
Mihai Farcas <farcasmihai91@gmail.com>
2023-02-22 18:47:17 +0200

    chore: linting and formatting

--- @app/client/vite.config.ts
+++ @app/client/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    port: 8000,
+    host: '0.0.0.0',
+    strictPort: true,
+  },
+});
