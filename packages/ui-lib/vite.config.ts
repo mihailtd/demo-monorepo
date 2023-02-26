5941338dbbc2b806cd18291aa6cf7b4430b72891
Mihai Farcas <farcasmihai91@gmail.com>
2023-02-26 17:40:51 +0200

    fix: empty build directory leads to error on client imports.

--- packages/ui-lib/vite.config.ts
+++ packages/ui-lib/vite.config.ts
@@ -6,6 +6,7 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
       formats: ['es'],
